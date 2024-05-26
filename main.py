21b4397beb11301bf834bc347656f6f88640db0d
tadeasf <taddy.fort@gmail.com>
2024-05-26 05:33:36 +0200

    improvements

--- main.py
+++ main.py
@@ -19,131 +19,67 @@ logging.basicConfig(
 )
 
 
-# CPU Benchmark: Fibonacci Calculation
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
-
-def fibonacci_benchmark(duration):
-    logging.info("Starting Fibonacci benchmark...")
-    print("Starting Fibonacci benchmark...")
+# Robust CPU Benchmark
+def cpu_benchmark(duration):
+    logging.info("Starting CPU benchmark...")
+    print("Starting CPU benchmark...")
     start_time = time.time()
     iterations = 0
+
+    def fibonacci(n):
+        if n <= 1:
+            return n
+        else:
+            return fibonacci(n - 1) + fibonacci(n - 2)
+
+    def is_prime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
     while time.time() - start_time < duration:
         fibonacci(30)
-        iterations += 1
-    logging.info("Fibonacci benchmark completed {} iterations".format(iterations))
-    print("Fibonacci benchmark completed {} iterations".format(iterations))
-    return iterations
-
-
-# CPU Benchmark: Sorting
-def sort_benchmark(duration, size):
-    logging.info("Starting Sorting benchmark...")
-    print("Starting Sorting benchmark...")
-    start_time = time.time()
-    iterations = 0
-    while time.time() - start_time < duration:
-        data = [random.random() for _ in range(size)]
+        data = [random.random() for _ in range(10000)]
         data.sort()
+        primes = [num for num in range(2, 1000) if is_prime(num)]
         iterations += 1
-    logging.info("Sorting benchmark completed {} iterations".format(iterations))
-    print("Sorting benchmark completed {} iterations".format(iterations))
+
+    logging.info("CPU benchmark completed {} iterations".format(iterations))
+    print("CPU benchmark completed {} iterations".format(iterations))
     return iterations
 
 
-# CPU Benchmark: Prime Number Calculation
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def prime_benchmark(duration, limit):
-    logging.info("Starting Prime Number benchmark...")
-    print("Starting Prime Number benchmark...")
-    start_time = time.time()
-    iterations = 0
-    while time.time() - start_time < duration:
-        primes = []
-        for num in range(2, limit):
-            if is_prime(num):
-                primes.append(num)
-        iterations += 1
-    logging.info("Prime Number benchmark completed {} iterations".format(iterations))
-    print("Prime Number benchmark completed {} iterations".format(iterations))
-    return iterations
-
-
-# CPU Benchmark: Factorial Calculation
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-def factorial_benchmark(duration):
-    logging.info("Starting Factorial benchmark...")
-    print("Starting Factorial benchmark...")
-    start_time = time.time()
-    iterations = 0
-    while time.time() - start_time < duration:
-        factorial(20)
-        iterations += 1
-    logging.info("Factorial benchmark completed {} iterations".format(iterations))
-    print("Factorial benchmark completed {} iterations".format(iterations))
-    return iterations
-
-
-def cpu_benchmark(duration):
-    fibonacci_iterations = fibonacci_benchmark(duration)
-    sort_iterations = sort_benchmark(duration, 10000)
-    prime_iterations = prime_benchmark(duration, 1000)
-    factorial_iterations = factorial_benchmark(duration)
-    return fibonacci_iterations, sort_iterations, prime_iterations, factorial_iterations
-
-
-# I/O Benchmark: File Read/Write
+# Robust I/O Benchmark
 def io_benchmark(duration):
     logging.info("Starting I/O benchmark...")
     print("Starting I/O benchmark...")
     filename = "test_io_benchmark.txt"
     data = "A" * (10**7)  # 10 MB of data
 
-    # Write Benchmark
     start_time = time.time()
     write_bytes = 0
+    read_bytes = 0
     while time.time() - start_time < duration:
         with open(filename, "w") as f:
             f.write(data)
         write_bytes += len(data)
-    logging.info("I/O write benchmark wrote {} bytes".format(write_bytes))
-    print("I/O write benchmark wrote {} bytes".format(write_bytes))
-
-    # Read Benchmark
-    start_time = time.time()
-    read_bytes = 0
-    while time.time() - start_time < duration:
         with open(filename, "r") as f:
             f.read()
         read_bytes += len(data)
+
+    logging.info("I/O write benchmark wrote {} bytes".format(write_bytes))
+    print("I/O write benchmark wrote {} bytes".format(write_bytes))
     logging.info("I/O read benchmark read {} bytes".format(read_bytes))
     print("I/O read benchmark read {} bytes".format(read_bytes))
 
-    # Clean up
     os.remove(filename)
-
     return write_bytes, read_bytes
 
 
-# Memory Benchmark: List Operations
+# Robust Memory Benchmark
 def memory_benchmark(duration):
     logging.info("Starting Memory benchmark...")
     print("Starting Memory benchmark...")
@@ -162,9 +98,7 @@ def main(duration):
     logging.info("Benchmark started at {}".format(start_time_total))
     print("Benchmark started at {}".format(start_time_total))
 
-    fibonacci_iterations, sort_iterations, prime_iterations, factorial_iterations = (
-        cpu_benchmark(duration)
-    )
+    cpu_iterations = cpu_benchmark(duration)
     write_bytes, read_bytes = io_benchmark(duration)
     memory_iterations = memory_benchmark(duration)
 
@@ -178,10 +112,7 @@ def main(duration):
 
     # Return results for logging
     return {
-        "fibonacci_iterations": fibonacci_iterations,
-        "sort_iterations": sort_iterations,
-        "prime_iterations": prime_iterations,
-        "factorial_iterations": factorial_iterations,
+        "cpu_iterations": cpu_iterations,
         "write_bytes": write_bytes,
         "read_bytes": read_bytes,
         "memory_iterations": memory_iterations,
